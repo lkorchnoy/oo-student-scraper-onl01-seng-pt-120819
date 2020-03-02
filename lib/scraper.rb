4180eefcb3c97bc9734518dbd7998e580815ec79
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-02 21:06:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,9 +16,9 @@ def self.scrape_index_page(index_url)
                 :location => location,
                 :profile_url => profile_url}
       students << student_info
-      end
+    end
     students
-   end
+end
 
 
  def self.scrape_profile_page(profile_url)
@@ -41,8 +41,8 @@ def self.scrape_index_page(index_url)
       student[:profile_quote] = page.css(".profile-quote").text
       student[:bio] = page.css("div.description-holder p").text
       student
-  end
-end
+  
+ end
 
 end
 
